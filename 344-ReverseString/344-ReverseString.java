55af24f39f837fd9447e5887e29740780c367acd
Kavin v s <145773639+Kavincodeg@users.noreply.github.com>
2026-07-14 14:02:10 +0530

    Create 344-ReverseString

---
+++ 344-ReverseString/344-ReverseString.java
@@ -0,0 +1,14 @@
+// Last updated: 14/07/2026, 14:02:11
+class Solution {
+    public void reverseString(char[] s) {
+        int i=0;
+        int j=s.length-1;
+        while(i<j){
+            char temp=s[i];
+            s[i]=s[j];
+            s[j]=temp;
+            i++;
+            j--;
+        }
+    }
+}
