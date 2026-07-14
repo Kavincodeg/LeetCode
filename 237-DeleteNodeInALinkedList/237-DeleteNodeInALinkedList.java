013255651d238c969a78baec2638a072337697fc
Kavin v s <145773639+Kavincodeg@users.noreply.github.com>
2026-07-14 14:02:19 +0530

    Create 237-DeleteNodeInALinkedList

---
+++ 237-DeleteNodeInALinkedList/237-DeleteNodeInALinkedList.java
@@ -0,0 +1,9 @@
+// Last updated: 14/07/2026, 14:02:19
+
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val=node.next.val;
+        node.next=node.next.next;
+        
+    }
+}
