1469cbf566e8d50a73f0d93ee1105bc1edcab5de
Kavin v s <145773639+Kavincodeg@users.noreply.github.com>
2026-07-27 11:46:13 +0530

    Create 912-SortanArray

---
+++ 912-SortanArray/version/java/912-SortanArray_20260727_114611.java
@@ -0,0 +1,8 @@
+// Last updated: 27/07/2026, 11:46:11
+1import java.util.*;
+2class Solution {
+3    public int[] sortArray(int[] nums) {
+4        Arrays.sort(nums);
+5        return nums;
+6    }
+7}
