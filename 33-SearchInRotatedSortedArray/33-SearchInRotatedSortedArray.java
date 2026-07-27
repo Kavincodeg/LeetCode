fe58374c1872a33f8781ce037cf229a5b1a32c2a
Kavin v s <145773639+Kavincodeg@users.noreply.github.com>
2026-07-27 11:50:41 +0530

    Create 33-SearchInRotatedSortedArray

---
+++ 33-SearchInRotatedSortedArray/33-SearchInRotatedSortedArray.java
@@ -0,0 +1,12 @@
+// Last updated: 27/07/2026, 11:50:41
+class Solution {
+    public int search(int[] nums, int target) {
+        int ind=-1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                ind=i;
+            }
+        }
+        return ind;
+    }
+}
