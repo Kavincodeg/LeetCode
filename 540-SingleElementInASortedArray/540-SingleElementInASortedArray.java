7af72e7fda09cabe8cd7df9d53d56d857f4962a0
Kavin v s <145773639+Kavincodeg@users.noreply.github.com>
2026-07-27 11:49:48 +0530

    Create 540-SingleElementInASortedArray

---
+++ 540-SingleElementInASortedArray/540-SingleElementInASortedArray.java
@@ -0,0 +1,15 @@
+// Last updated: 27/07/2026, 11:49:47
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        for(int key:map.keySet()){
+            if(map.get(key)==1){
+                return key;
+            }
+        }
+        return 0;
+    }
+}
